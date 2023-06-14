93c867ff0c64abdfc21cdde800258fc0457799e7
ZZWSD <125881299+ZZWSD@users.noreply.github.com>
2023-06-14 17:58:51 +0800

    計算機練習 4
    1.等於按鍵事件綁定
    2.小數點按鈕程式碼
    3.清除按鍵程式碼

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -130,5 +130,50 @@ namespace Calculator
             operators = 0; //選擇「加」號
         }
 
+        private void btnDot_Click(object sender, RoutedEventArgs e)
+        {
+            // 確認輸入文字框中完全沒有小數點
+            if (txtNumber.Text.IndexOf(".") == -1)
+                txtNumber.Text = txtNumber.Text + ".";
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Text = "0";
+            firstNumber = 0f;
+            secondNumber = 0f;
+            operators = -1;
+        }
+
+        private void btnEqual_Click(object sender, RoutedEventArgs e)
+        {
+            float finalResults = 0f; //宣告最後計算結果變數
+            secondNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第二個數字的全域變數
+
+            //依照四則運算符號的選擇，進行加減乘除
+            switch (operators)
+            {
+                case 0:
+                    finalResults = firstNumber + secondNumber;
+                    break;
+                case 1:
+                    finalResults = firstNumber - secondNumber;
+                    break;
+                case 2:
+                    finalResults = firstNumber * secondNumber;
+                    break;
+                case 3:
+                    finalResults = firstNumber / secondNumber;
+                    break;
+            }
+
+            txtNumber.Text = string.Format("{0:0.##########}", finalResults); //在輸入文字框中，顯示最後計算結果，並且轉換成格式化的字串內容
+
+            //重置所有全域變數
+            firstNumber = 0f;
+            secondNumber = 0f;
+            operators = -1;
+        }
+
     }
 }
